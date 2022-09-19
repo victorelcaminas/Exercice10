cc9131cb9756f873334589a53d782074ec28a4a8
alu10211999 <alumar1999@ieselcaminas.org>
2022-09-19 15:41:52 +0200

    Initial commit

---
+++ src/Exercice10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercice10 {
+
+    public static void main(String[] args) {
+
+        int x;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Introduce un numero:");
+        x = input.nextInt();
+
+        if (x>=0) {
+            System.out.println("El numero es positivo");
+        }
+        else {
+            System.out.println("El numero es negativo");
+        }
+
+    }
+}
